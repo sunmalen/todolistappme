992c9fbabf7531430a0da2f30c689dac2bca6312
sun malen <sunmalen@gmail.com>
2024-01-09 12:53:32 +0700

    update app router

--- lib/services/app_router.dart
+++ lib/services/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:todouserapp/screens/pending_tasks_screen.dart';
 import 'package:todouserapp/screens/tabs_screen.dart';
 
 import '../screens/recycle_bin_screen.dart';
@@ -13,6 +14,9 @@ class AppRouter{
       case TabsScreen.id:
         return MaterialPageRoute(
             builder: (_)=> const TabsScreen());
+      case RecycleBin.id:
+        return MaterialPageRoute(
+            builder: (_) => const PendingTasksScreen());
       default:
         return null;
     }
